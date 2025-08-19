b38e62daaa850986b5fed1bc52494845cce6179e
PeymonA <peymon.armand4@gmail.com>
2025-08-20 11:48:03 +1200

    API 1 passing test

--- Mission-2-testing/myapp/tests/app.test.js
+++ Mission-2-testing/myapp/tests/app.test.js
@@ -10,28 +10,34 @@ describe('GET /', () => {
 
 describe('GET /carValue', () => {
   test("Returns correct output", async() =>{
-    const response = await request(app).get('/carValue?model=Civic&year=2014')
+    const response = await request(app).get('/getValue?model=Civic&year=2014')
     expect(response.body.message).toMatch("6614")
   })
 
   test("Doesn't accept negative year", async() =>{
-    const response = await request(api).get('/getValue?model=Civic&year=-1')
+    const response = await request(app).get('/getValue?model=Civic&year=-1')
     expect(response.status).toBe(400)
   })
 
   test("Doesn't accept non integer years", async() =>{
-    const response = await request(api).get('/getValue?model=Civic&year=202.5')
+    const response = await request(app).get('/getValue?model=Civic&year=two')
     expect(response.status).toBe(400)
   })
 
   test("Numbers only for model", async() =>{
-    const response = await request(api).get('/getValue?model=911&year=2020')
+    const response = await request(app).get('/getValue?model=911&year=2020')
     expect(response.body.message).toMatch("2020")
   })
 
   test("Checking that symbols are ignored", async() =>{
-    const response = await request(api).get('/getValue?model=Civic!&year=2014')
+    const response = await request(app).get('/getValue?model=Civic!&year=2014')
     expect(response.body.message).toMatch("6614")
   })
-
 })
+
+describe('GET /getRiskRating', () => {
+  it('responds with 200', async () => {
+    const res = await request(app).get('/');
+    expect(res.status).toEqual(200);
+  });
+});
